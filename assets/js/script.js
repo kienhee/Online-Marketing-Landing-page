6bb0c52ace422b0b8332b83529c1dca048557dda
Kienhee <kienhee.it@gmail.com>
2022-02-19 10:16:46 +0700

    fix navbar - test divece Signed-off-by: Kienhee <kienhee.it@gmail.com>

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 let navMobile = document.querySelector("#nav-mobile");
 let NavMenu = document.querySelector(".nav-menu");
 let heightHeader = document.querySelector(".header");
-heightHeader = heightHeader.clientHeight
-console.log(heightHeader);
+heightHeader = heightHeader.clientHeight;
+let $$ = document.querySelectorAll.bind(document);
+const NavItem = $$('.nav-item');
+console.log(NavItem);
+
 navMobile.addEventListener("click", function () {
-    // NavMenu.style.top = `80 px`;
     NavMenu.classList.toggle("show-mobile");
+
+})
+NavItem.forEach(function (item, index) {
+    item.onclick = function () {
+        NavMenu.classList.toggle("show-mobile");
+    }
 })
