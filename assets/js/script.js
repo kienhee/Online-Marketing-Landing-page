5764231ade66061513644aef4404cbbc34a0ee71
Kienhee <kienhee.it@gmail.com>
2022-02-19 09:33:07 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+let navMobile = document.querySelector("#nav-mobile");
+let NavMenu = document.querySelector(".nav-menu");
+let heightHeader = document.querySelector(".header");
+heightHeader = heightHeader.clientHeight
+console.log(heightHeader);
+navMobile.addEventListener("click", function () {
+    NavMenu.style.top = `80 px`;
+
+    NavMenu.classList.toggle("show-mobile");
+})
