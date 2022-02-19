a044530b0c1a09d14323cb000903f313cb6aef26
Kienhee <kienhee.it@gmail.com>
2022-02-19 22:40:35 +0700

    done all

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,42 @@
+
+$(document).ready(function () {
+    $('.blog-container').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        // autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-circle-chevron-left icon-slider' aria-hidden='true'></i></button>",
+        nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-circle-chevron-right icon-slider' aria-hidden='true'></i></button>",
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    arrows: true,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                }
+            }]
+    });
+});
+
 let navMobile = document.querySelector("#nav-mobile");
 let NavMenu = document.querySelector(".nav-menu");
 let heightHeader = document.querySelector(".header");
@@ -14,4 +53,4 @@ NavItem.forEach(function (item, index) {
     item.onclick = function () {
         NavMenu.classList.toggle("show-mobile");
     }
-})
+}) 
