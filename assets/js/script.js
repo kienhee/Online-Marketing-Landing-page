50f251e12efac4ab6520ee263124d36b099fba7d
Kienhee <kienhee.it@gmail.com>
2022-02-19 09:48:59 +0700

    fix navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,6 @@ let heightHeader = document.querySelector(".header");
 heightHeader = heightHeader.clientHeight
 console.log(heightHeader);
 navMobile.addEventListener("click", function () {
-    NavMenu.style.top = `80 px`;
-
+    // NavMenu.style.top = `80 px`;
     NavMenu.classList.toggle("show-mobile");
 })
